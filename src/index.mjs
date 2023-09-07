1118f7d6185ae749b987fe7200ab2f6d14326a61
diegoabad <diegoabad.2289@gmail.com>
2023-09-07 11:42:12 -0300

    dotenv

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,16 @@
 // Importa 'router' correctamente
 import { router } from './routes/scrapperRouter.mjs';
-
+import { config } from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-
+config();
 const app = express();
-
+const PORT = process.env.PORT 
 app.use(cors());
 app.use(express.json());
 // Define la ruta '/scrapper' y utiliza 'router'
 app.use('/scrapper', router);
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log("Servidor OK");
 });
